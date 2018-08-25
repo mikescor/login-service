64c7bc60d8dd6e17c966b26ce60b21b96de6ba23
mikescor <mykytyn.mykhailo@gmail.com>
2018-08-25 18:09:01 +0300

    Update secret key

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY=os.environ.get('SECRET_KEY') or \
+               '\xa8\x92+\xca\xb5\xd6)jX\x80\xd7]+\x031\xf7\xac\xed\xb4{t;\xa0'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USERS_PER_PAGE = 5
